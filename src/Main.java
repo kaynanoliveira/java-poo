59420c30e8226cdc3d07eb638155fa587da37dd5
kayna <kaynanoliveira357@gmail.com>
2025-07-18 16:35:35 -0300

    classe Personagem

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Personagem heroi = new Personagem();
+        heroi.nome = "Homem de ferro";
+        heroi.nivel = 1;
+        heroi.forca = 20;
 
-        // Declarando um objeto do tipo celular => Instanciando um objeto
-        Celular iphone = new Celular();
-        iphone.nome = "Iphone 12";
-        iphone.armazenamento = 256;
-        iphone.sistemaOperacional = "iOS";
-        iphone.tamanhoTela = 6.2f;
-
-        System.out.printf("Celular %s com tela de %.1f, com %dgb e SO %s", iphone.nome,
-                iphone.tamanhoTela, iphone.armazenamento, iphone.sistemaOperacional);
+        heroi.mostrarStatus();
+        heroi.atacar("Thanos", "Espada");
     }
 }
---
+++ src/Personagem.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Personagem {
+    String nome;
+    int nivel;
+    int forca;
+
+    int calcularDano(){
+        Random gerador = new Random();
+        int dado20Faces = 1 + gerador.nextInt(19);
+        int dano = forca + dado20Faces;
+        return dano;
+    }
+
+    void atacar(String inimigo, String arma){
+        int danoCausado = calcularDano();
+        if(arma.isEmpty()){
+            System.out.printf("%s atacou %s e causou %d de dano",
+                    nome, inimigo, danoCausado);
+        } else {
+            System.out.printf("%s usou '%s' para atacar %s e causou %d de dano",
+                    nome, arma, inimigo, danoCausado);
+        }
+    }
+
+    void mostrarStatus(){
+        System.out.printf("Personagem: %s no nivel (lvl: %d) com %d de força \n", nome, nivel, forca);
+    }
+}
